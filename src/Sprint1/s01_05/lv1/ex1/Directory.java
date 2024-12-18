1cd9b15baa79cfb65f1da98eab27c920493b3c90
carlos <carlosohunter@gmail.com>
2024-12-18 12:17:01 +0100

    s01_05 Java Utils

--- src/Sprint1/s01_05/lv1/ex1/Directory.java
+++ src/Sprint1/s01_05/lv1/ex1/Directory.java
@@ -1,72 +1,111 @@
 package Sprint1.s01_05.lv1.ex1;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Scanner;
 
-public class Directory {
+public class Directory implements Serializable {
     private final File directory;
-    private FileWriter myWriter;
+    transient private FileWriter myWriter;
 
     public Directory(String dir) {
         directory = new File(dir);
-
-        try {
-            File save = new File("output/" + directory.getName());
-            myWriter = new FileWriter(save);
-        } catch (IOException e) {
-            System.out.println("Cannot write to file. ");
-        }
     }
 
-    public void list(File dir, int depth) {
-        if(dir.exists() && dir.isDirectory()) {
-            File[] files = dir.listFiles();
-            if(files != null) {
-                Arrays.sort(files, Comparator.comparing(File::getName));
+    /**
+     * Prints directory to console and to output file
+     * @param dir to list recursively
+     * @param depth used for formatting
+     */
+    private void list(File dir, int depth) {
+        File save = new File("output/" + directory.getName());
+        boolean printToFile = Files.isWritable(save.toPath());
 
-                try {
+        try {
+            if(depth == 0) {
+                if(printToFile) myWriter = new FileWriter(save);
+            }
+            if(dir.exists() && dir.isDirectory()) {
+                File[] files = dir.listFiles();
+                if(files != null) {
+                    Arrays.sort(files, Comparator.comparing(File::getName));
+
                     if(depth == 0){
-                        String msg = " ".repeat(30)+"Last Modified";
-                        myWriter.write(msg+"\n");
+                        String msg = " ".repeat(40)+"Last Modified";
+                        if(printToFile) myWriter.write(msg+"\n");
                         System.out.println(msg);
                     }
                     for (File file : files) {
 
-                        StringBuilder str = new StringBuilder();
+                        StringBuilder strb = new StringBuilder();
 
                         String signature = file.isFile() ? " (F) " : " (D) ";
                         String lastModified = (new Timestamp(file.lastModified())).toString();
-                        str.append("  ".repeat(depth));
-                        str.append(file.getName());
-                        str.append(" ".repeat(Math.max(30 - str.length(), 2)));
-                        str.append(signature);
-                        str.append(" ".repeat(5));
-                        str.append(lastModified);
+                        strb.append("  ".repeat(depth));
+                        strb.append(file.getName());
+                        strb.append(" ".repeat(Math.max(30 - strb.length(), 2)));
+                        strb.append(signature);
+                        strb.append(" ".repeat(5));
+                        strb.append(lastModified);
 
-                        System.out.println(str.toString());
-                        myWriter.write(str.append("\n").toString());
+                        System.out.println(strb.toString());
+                        if(printToFile) myWriter.write(strb.append("\n").toString());
 
                         if(file.isDirectory()) {
                             list(new File(file.getPath()), depth+1);
                         }
                     }
-                    myWriter.close();
-                } catch (IOException e) {
-                    System.out.println("Cannot write to file. "); // todo debug
                 }
-            } //else System.out.println("Directory is empty.");
-        } else System.out.println("Not a directory.");
+            }
+            if(depth == 0) {
+                myWriter.write("\n");
+                System.out.println();
+
+                if(printToFile) myWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot print list to file. ");
+        }
     }
     public void list() {
         list(directory, 0);
     }
 
     public void readTxt(String fileName) {
-        //File txt = new File(directory.)
+
+        if(Files.notExists(Path.of(fileName))) {
+            String[] name_ext = fileName.split(".");
+            if(name_ext.length == 1) {
+                if(Files.exists(Path.of(fileName+".txt"))) {
+                    fileName = fileName+".txt";
+                }
+            }
+        }
+
+        File txt = new File(fileName);
+
+        try {
+            Scanner myReader = new Scanner(txt);
+            System.out.println(fileName+":");
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+            System.out.println();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. ");
+        }
+
     }
+
+    public Path getPath() {
+        return directory.toPath();
+    }
+
 }
