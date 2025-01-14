e54396641b465834d8e9a223bd8b5c44eb0c0ac2
carlos <carlosohunter@gmail.com>
2025-01-14 12:36:44 +0100

    s01_06 Generics

--- src/Sprint1/s01_06/lv3/Main.java
+++ src/Sprint1/s01_06/lv3/Main.java
@@ -3,6 +3,7 @@ package Sprint1.s01_06.lv3;
 public class Main {
     public static void main(String[] args) {
         Smartphone s = new Smartphone();
+        s.takePhotos();
         Generic.genericS(s); // smartphone calls
         Generic.genericT(s); // smartphone calls
     }
