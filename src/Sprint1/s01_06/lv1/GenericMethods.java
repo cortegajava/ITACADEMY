225e19885eb437d93a32ae3575d5131e6850f41a
carlos <carlosohunter@gmail.com>
2025-01-16 12:04:02 +0100

    using maven now, minor fixes and TODOs

--- src/Sprint1/s01_06/lv1/GenericMethods.java
+++
@@ -1,9 +0,0 @@
-package Sprint1.s01_06.lv1;
-
-public class GenericMethods {
-    public static <T> void printTriple(T... params) {
-        System.out.println(params[0]);
-        System.out.println(params[1]);
-        System.out.println(params[2]);
-    }
-}
--- src/Sprint1/s01_06/lv3/Telephone.java
+++
@@ -1,5 +0,0 @@
-package Sprint1.s01_06.lv3;
-
-public interface Telephone {
-    void call();
-}
